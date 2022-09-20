216f8cf8df94c6d8092476c48b7b4211eecae445
t-visor <tkimbr1@students.towson.edu>
2022-09-20 10:54:51 -0400

    further modified code for processing a facescrub images

--- run-on-dataset.py
+++ run-on-dataset.py
@@ -1,28 +1,61 @@
 #!/usr/bin/env python3
 
 from glob import glob
+import argparse
 import sys
 import subprocess
 import os
 
-subdirectories = glob('inputs/whole_imgs/facescrub-actresses-lowkey/*', recursive = True)
 
-print('---FOLDERS FOUND---')
-for folder in subdirectories:
-    print(folder)
-print('-------------------\n')
+def main():
+    """
+        Run face restoration on an entire dataset of face images.
+    """
+    arguments = parse_command_line_arguments()
+    source_directory = arguments.source_directory[0]
 
-for folder in subdirectories:
-    print('Processing folder:', folder)
-    subprocess.call(['python3',
-                     'inference_gfpgan.py',
-                     '-i',
-                     folder,
-                     '-o',
-                     'results/' + os.path.basename(folder),
-                     '-v',
-                     '1.3',
-                     '-s',
-                     '2'])
+    #subdirectories = glob('inputs/whole_imgs/facescrub-actresses-lowkey/*', recursive = True)
+    subdirectories = glob('inputs/whole_imgs/' + source_directory + '/*', recursive = True)
 
-# python3 inference_gfpgan.py -i inputs/whole_imgs/facescrub-actresses-lowkey/Adrianne_León/ -o results/Adrianne_Leon -v 1.3 -s 2 
+    print('---FOLDERS FOUND---')
+    for folder in subdirectories:
+        print(folder)
+    print('-------------------\n')
+
+    for folder in subdirectories:
+        print('Processing folder:', folder)
+        subprocess.call(['python3',
+                         'inference_gfpgan.py',
+                         '-i',
+                         folder,
+                         '-o',
+                         'results/' + source_directory + '/' + os.path.basename(folder),
+                         '-v',
+                         '1.3',
+                         '-s',
+                         '2'])
+
+
+def parse_command_line_arguments() -> argparse.ArgumentParser:
+    """
+        Parse the arguments from the command-line.
+        If no arguments are passed, the help screen will
+        be shown and the program will be terminated.
+
+    Returns:
+        (argparse.ArgumentParser): the parser with command-line arguments
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--source_directory', nargs=1, required=True,
+                        help='Directory with images of people under "input/whole_imgs".')
+
+    # if no arguments were passed, show the help screen
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+
+    return parser.parse_args()
+
+
+main()
